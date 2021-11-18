59d30d14066d888988e58ad0b972ffaf4b31d7c4
juanquiceno2000 <juan.quiceno@yuxiglobal.com>
2021-11-18 17:40:12 -0500

    Login Feature

--- Steps/LoginSteps.cs
+++ Steps/LoginSteps.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SpecflowBDD.PageObject;
+using System;
 using TechTalk.SpecFlow;
 
 namespace SpecflowBDD.Steps
@@ -6,40 +8,50 @@ namespace SpecflowBDD.Steps
     [Binding]
     public class LoginSteps
     {
+        LoginPage loginPage = new LoginPage();
+        HomePage swagLabsHomePage = new HomePage();
+        bool result = false;
+
+        #region Given
+
         [Given(@"a user in the Login Page")]
         public void GivenAUserInTheLoginPage()
         {
-            
+            Assert.IsTrue(loginPage.IsLogoCompanyPresent());
         }
-        
+
+        #endregion
+
+        #region When
+
         [When(@"the user enters an incorrect UserName")]
         public void WhenTheUserEntersAnIncorrectUserName()
         {
-            
+            loginPage.typeInUserFieldIncorrectName();
         }
         
         [When(@"the user enters expected Password")]
         public void WhenTheUserEntersExpectedPassword()
         {
-            
+            loginPage.typeInPasswordFieldExpectedPassword();
         }
         
         [When(@"click on Login Button")]
         public void WhenClickOnLoginButton()
         {
-            
+            swagLabsHomePage = loginPage.clickOnLoginButton();
         }
         
         [When(@"the user enters expected UserName")]
         public void WhenTheUserEntersExpectedUserName()
         {
-            
+            loginPage.typeInUserFieldExpectedName();
         }
         
         [When(@"the user enters an incorrect Password")]
         public void WhenTheUserEntersAnIncorrectPassword()
         {
-            
+            loginPage.typeInPasswordFieldIncorrectPassword();
         }
         
         [When(@"the user doesn't enter UserName in the Username field")]
@@ -53,17 +65,34 @@ namespace SpecflowBDD.Steps
         {
             
         }
-        
+
+        #endregion
+
+        #region Then
+
         [Then(@"the system throws the error ""(.*)"" message")]
-        public void ThenTheSystemThrowsTheErrorMessage(string p0)
+        public void ThenTheSystemThrowsTheErrorMessage(string loginErrorMessage)
         {
-            
+            if (loginPage.getTextLoginErrorMessage() == loginErrorMessage)
+            {
+                result = true;
+            }
+            Assert.IsTrue(result);
+            result = false;
         }
-        
+
+
         [Then(@"the system grant access by leaving the user on the Home Page in ""(.*)"" section")]
-        public void ThenTheSystemGrantAccessByLeavingTheUserOnTheHomePageInSection(string p0)
+        public void ThenTheSystemGrantAccessByLeavingTheUserOnTheHomePageInSection(string tittleHomePage)
         {
-            
+            if (swagLabsHomePage.getTextProductsTittleHomePage() == tittleHomePage)
+            {
+                result = true;
+            }
+            Assert.IsTrue(result);
+            result = false;
         }
+
+        #endregion
     }
 }
