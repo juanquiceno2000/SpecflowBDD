3a9098a61826bb9e8d5c7d5a40b2451d04cb0786
juanquiceno2000 <juan.quiceno@yuxiglobal.com>
2021-11-19 10:19:25 -0500

    Adding RestSharp Tests

---
+++ Features/API.feature
@@ -0,0 +1,16 @@
+﻿Feature: API
+	Verify APIs with RestSharp
+
+
+@mytag
+Scenario: 00) GET Method
+	Given a user in the Login Page
+	When sends Get Method
+	Then the response code should be 200
+
+
+@mytag
+Scenario: 01) POST Method
+	Given a user in the Login Page
+	When sends Post Method
+	Then the response code should be 200
---
+++ POCO/JsonBodyRequests/CreateBooking.cs
@@ -0,0 +1,43 @@
+﻿using RestSharp;
+using System;
+
+
+namespace SpecflowBDD.POCO.JsonBodyRequests
+{
+    public class CreateBooking
+    {
+        private string postBrandNewBooking = "https://restful-booker.herokuapp.com/booking";
+
+        string createBrandNewBooking = "{ \r\n" +
+                                          "\"firstname\": \"JQ\",\r\n" +
+                                          "\"lastname\": \"Test\",\r\n" +
+                                          "\"totalprice\": \"4000\",\r\n" +
+                                          "\"depositpaid\": \"true\",\r\n" +
+                                          "\"bookingdates\":\r\n" +
+                                            "{\r\n" +
+                                                "\"checkin\": \"2018-01-01\",\r\n" +
+                                                "\"checkout\": \"2019-01-01\"\r\n" +
+                                            "},\r\n" +
+                                           "\"additionalneeds\": \"Breakfast\"\r\n" +
+                                         "}";
+
+
+        public int CreateBrandNewBooking()
+        {
+            RestClient restClient = new RestClient();
+            RestRequest request = new RestRequest()
+            {
+                Resource = postBrandNewBooking,
+                Method = Method.POST,
+                RequestFormat = DataFormat.Json
+            };
+
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Accept", "*/*");
+            request.AddJsonBody(createBrandNewBooking);
+
+            IRestResponse response = restClient.Execute(request);
+            return (int)response.StatusCode;
+        }
+    }
+}
